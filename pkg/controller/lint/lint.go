7bcd2285569f84b3691819989b006becabc7c54d
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-12-23 23:14:51 +0900

    refactor: split functions (#104)

--- pkg/controller/lint/lint.go
+++ pkg/controller/lint/lint.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/lintnet/lintnet/pkg/config"
+	"github.com/lintnet/lintnet/pkg/errlevel"
 	"github.com/lintnet/lintnet/pkg/jsonnet"
 	"github.com/lintnet/lintnet/pkg/module"
 	"github.com/sirupsen/logrus"
@@ -22,7 +23,7 @@ type ParamLint struct {
 	OutputSuccess  bool
 }
 
-func (c *Controller) Lint(ctx context.Context, logE *logrus.Entry, param *ParamLint) error { //nolint:cyclop
+func (c *Controller) Lint(ctx context.Context, logE *logrus.Entry, param *ParamLint) error {
 	cfg := &config.Config{}
 	if err := c.findAndReadConfig(param.ConfigFilePath, cfg); err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
@@ -45,36 +46,58 @@ func (c *Controller) Lint(ctx context.Context, logE *logrus.Entry, param *ParamL
 		return err
 	}
 
-	errLevel := errorLevel
-	if param.ErrorLevel != "" {
-		ll, err := newErrorLevel(param.ErrorLevel)
-		if err != nil {
-			return err
-		}
-		errLevel = ll
+	errLevel, err := c.getErrorLevel(param.ErrorLevel)
+	if err != nil {
+		return err
 	}
 
-	results := make(map[string]*FileResult, len(targets))
-	for _, target := range targets {
-		jsonnetAsts, err := c.readJsonnets(target.LintFiles)
-		if err != nil {
-			return err
-		}
-		for _, dataFile := range target.DataFiles {
-			rs, err := c.lint(dataFile, jsonnetAsts)
-			if err != nil {
-				results[dataFile] = &FileResult{
-					Error: err.Error(),
-				}
-				continue
-			}
-			results[dataFile] = &FileResult{
-				Results: rs,
-			}
-		}
+	results, err := c.getResults(targets)
+	if err != nil {
+		return err
 	}
 
 	return c.Output(logE, cfg, errLevel, results, param.Outputs, param.OutputSuccess)
+}
+
+func (c *Controller) getResults(targets []*Target) (map[string]*FileResult, error) {
+	results := make(map[string]*FileResult, len(targets))
+	for _, target := range targets {
+		if err := c.lintTarget(target, results); err != nil {
+			return nil, err
+		}
+	}
+	return results, nil
+}
+
+func (c *Controller) lintTarget(target *Target, results map[string]*FileResult) error {
+	jsonnetAsts, err := c.readJsonnets(target.LintFiles)
+	if err != nil {
+		return err
+	}
+	for _, dataFile := range target.DataFiles {
+		rs, err := c.lint(dataFile, jsonnetAsts)
+		if err != nil {
+			results[dataFile] = &FileResult{
+				Error: err.Error(),
+			}
+			continue
+		}
+		results[dataFile] = &FileResult{
+			Results: rs,
+		}
+	}
+	return nil
+}
+
+func (c *Controller) getErrorLevel(errorLevel string) (errlevel.Level, error) {
+	if errorLevel == "" {
+		return errlevel.Error, nil
+	}
+	ll, err := errlevel.New(errorLevel)
+	if err != nil {
+		return ll, err //nolint:wrapcheck
+	}
+	return ll, nil
 }
 
 func (c *Controller) lint(dataFile string, jsonnetAsts map[string]jsonnet.Node) (map[string]*Result, error) {
