ffe216589808f547f9cd7113639e86458bf0b8f0
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-12-28 10:49:26 +0900

    test: add tests to encoding (#130)

--- pkg/encoding/plain.go
+++ pkg/encoding/plain.go
@@ -3,5 +3,5 @@ package encoding
 type plainUnmarshaler struct{}
 
 func (c *plainUnmarshaler) Unmarshal(_ []byte) (interface{}, error) {
-	return map[string]interface{}{}, nil
+	return nil, nil //nolint:nilnil
 }
