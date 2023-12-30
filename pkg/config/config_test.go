9f990ebcf065c9d6a9ad21129be6c0b579c2d83b
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-12-30 09:31:51 +0900

    refactor: rename fields (#140)
    * refactor: rename fields
    
    * test: add tests
    
    * fix: ignore a lint error

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/lintnet/lintnet/pkg/config"
+	"github.com/lintnet/lintnet/pkg/errlevel"
 )
 
 func TestRawModule_UnmarshalJSON(t *testing.T) {
@@ -54,6 +55,151 @@ func TestRawModule_UnmarshalJSON(t *testing.T) {
 				t.Fatal("error must be returned")
 			}
 			if diff := cmp.Diff(d.exp, m); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
+
+func TestRawConfig_Parse(t *testing.T) { //nolint:funlen
+	t.Parallel()
+	data := []struct {
+		name   string
+		rawCfg *config.RawConfig
+		cfg    *config.Config
+		isErr  bool
+	}{
+		{
+			name: "normal",
+			rawCfg: &config.RawConfig{
+				ErrorLevel: "info",
+				Targets: []*config.RawTarget{
+					{
+						LintGlobs: []*config.LintGlob{
+							{
+								Glob: "yoo/*.jsonnet",
+							},
+							{
+								Glob: "zoo/*.jsonnet",
+								Param: map[string]any{
+									"limit": 10,
+								},
+							},
+						},
+						Modules: []*config.RawModule{
+							{
+								Glob: "github.com/suzuki-shunsuke/example-lintnet-modules/foo/*.jsonnet@0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+								Param: map[string]any{
+									"excluded": []string{"test"},
+								},
+							},
+							{
+								Glob: "github.com/suzuki-shunsuke/example-lintnet-modules/bar/*.jsonnet@0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+							},
+						},
+						DataFiles: []string{
+							".github/workflows/*.yml",
+							".github/workflows/*.yaml",
+							"!.github/workflows/excluded.yaml",
+						},
+					},
+				},
+			},
+			cfg: &config.Config{
+				ErrorLevel: errlevel.Info,
+				Targets: []*config.Target{
+					{
+						LintFiles: []*config.ModuleGlob{
+							{
+								ID:        "yoo/*.jsonnet",
+								SlashPath: "yoo/*.jsonnet",
+							},
+							{
+								ID:        "zoo/*.jsonnet",
+								SlashPath: "zoo/*.jsonnet",
+								Param: map[string]any{
+									"limit": 10,
+								},
+							},
+						},
+						DataFiles: []string{
+							".github/workflows/*.yml",
+							".github/workflows/*.yaml",
+							"!.github/workflows/excluded.yaml",
+						},
+						Modules: []*config.ModuleGlob{
+							{
+								ID:        "github.com/suzuki-shunsuke/example-lintnet-modules/foo/*.jsonnet@0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+								SlashPath: "github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325/foo/*.jsonnet",
+								Archive: &config.ModuleArchive{
+									ID:        "github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+									Type:      "github",
+									Host:      "github.com",
+									RepoOwner: "suzuki-shunsuke",
+									RepoName:  "example-lintnet-modules",
+									Ref:       "0ed62adf055a4fbd7ef7ebe304f01794508ed325",
+									Tag:       "v0.1.3",
+								},
+								Param: map[string]any{
+									"excluded": []string{"test"},
+								},
+							},
+							{
+								ID:        "github.com/suzuki-shunsuke/example-lintnet-modules/bar/*.jsonnet@0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+								SlashPath: "github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325/bar/*.jsonnet",
+								Archive: &config.ModuleArchive{
+									ID:        "github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+									Type:      "github",
+									Host:      "github.com",
+									RepoOwner: "suzuki-shunsuke",
+									RepoName:  "example-lintnet-modules",
+									Ref:       "0ed62adf055a4fbd7ef7ebe304f01794508ed325",
+									Tag:       "v0.1.3",
+								},
+							},
+						},
+						ModuleArchives: map[string]*config.ModuleArchive{
+							"github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3": {
+								ID:        "github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+								Type:      "github",
+								Host:      "github.com",
+								RepoOwner: "suzuki-shunsuke",
+								RepoName:  "example-lintnet-modules",
+								Ref:       "0ed62adf055a4fbd7ef7ebe304f01794508ed325",
+								Tag:       "v0.1.3",
+							},
+						},
+					},
+				},
+				ModuleArchives: map[string]*config.ModuleArchive{
+					"github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3": {
+						ID:        "github.com/suzuki-shunsuke/example-lintnet-modules/0ed62adf055a4fbd7ef7ebe304f01794508ed325:v0.1.3",
+						Type:      "github",
+						Host:      "github.com",
+						RepoOwner: "suzuki-shunsuke",
+						RepoName:  "example-lintnet-modules",
+						Ref:       "0ed62adf055a4fbd7ef7ebe304f01794508ed325",
+						Tag:       "v0.1.3",
+					},
+				},
+			},
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			t.Parallel()
+			cfg, err := d.rawCfg.Parse()
+			if err != nil {
+				if d.isErr {
+					return
+				}
+				t.Fatal(err)
+			}
+			if d.isErr {
+				t.Fatal("error must be returned")
+			}
+			if diff := cmp.Diff(d.cfg, cfg); diff != "" {
 				t.Fatal(diff)
 			}
 		})
