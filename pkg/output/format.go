b5ae0ae55616898ca6ae8f0ee6cdfa1788c20381
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-01-14 21:19:49 +0900

    fix: skip validation if error level is empty

--- pkg/output/format.go
+++ pkg/output/format.go
@@ -17,14 +17,21 @@ func FormatResults(logE *logrus.Entry, results []*domain.Result, errLevel errlev
 	list := make([]*domain.Error, 0, len(results))
 	for _, result := range results {
 		for _, fe := range result.FlatErrors() {
-			el, err := errlevel.New(fe.Level)
-			if err != nil {
-				logE.WithError(err).WithFields(logrus.Fields{
-					"lint_file":   fe.LintFile,
-					"error_level": fe.Level,
-				}).Warn("error level is invalid")
+			el := errlevel.Error
+			invalid := false
+			if fe.Level != "" {
+				e, err := errlevel.New(fe.Level)
+				if err != nil {
+					logE.WithError(err).WithFields(logrus.Fields{
+						"lint_file":   fe.LintFile,
+						"error_level": fe.Level,
+					}).Warn("error level is invalid")
+					invalid = true
+				} else {
+					el = e
+				}
 			}
-			if err != nil || el >= errLevel {
+			if invalid || el >= errLevel {
 				list = append(list, fe)
 			}
 		}
