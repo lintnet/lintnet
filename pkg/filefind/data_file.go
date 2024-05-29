56ba0a9e317d50a90eaa3e4a0b1b8b5364eb3646
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-05-30 08:16:29 +0900

    feat: add base_data_path

---
+++ pkg/filefind/data_file.go
@@ -0,0 +1,45 @@
+package filefind
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+
+	"github.com/bmatcuk/doublestar/v4"
+	"github.com/lintnet/lintnet/pkg/config"
+	"github.com/lintnet/lintnet/pkg/domain"
+	"github.com/spf13/afero"
+)
+
+func (f *FileFinder) findDataFiles(dataBasePath string, files []*config.DataFile, cfgDir string, ignorePatterns []string) ([][]*domain.Path, error) {
+	if dataBasePath == "" {
+		dataFiles, err := f.findFilesFromPaths(files, cfgDir, ignorePatterns)
+		if err != nil {
+			return nil, err
+		}
+		return [][]*domain.Path{dataFiles}, nil
+	}
+
+	matches := map[string]struct{}{}
+	if err := doublestar.GlobWalk(afero.NewIOFS(f.fs), filepath.Join(cfgDir, filepath.FromSlash(dataBasePath)), func(path string, d fs.DirEntry) error {
+		if err := ignorePath(path, ignorePatterns); err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			path = filepath.Dir(path)
+		}
+		matches[path] = struct{}{}
+		return nil
+	}, doublestar.WithNoFollow()); err != nil {
+		return nil, fmt.Errorf("search files: %w", err)
+	}
+	paths := make([][]*domain.Path, 0, len(matches))
+	for rootPath := range matches {
+		dataFiles, err := f.findFilesFromPaths(files, rootPath, ignorePatterns)
+		if err != nil {
+			return nil, err
+		}
+		paths = append(paths, dataFiles)
+	}
+	return paths, nil
+}
