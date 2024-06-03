fe2a2eaaf88f94be4a6bbedd5a9ffc4edb815927
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-06-03 17:17:29 +0900

    feat: add native functions

--- pkg/jsonnet/vm.go
+++ pkg/jsonnet/vm.go
@@ -2,18 +2,44 @@ package jsonnet
 
 import (
 	"github.com/google/go-jsonnet"
+	"github.com/lintnet/go-jsonnet-native-functions/pkg/net/url"
+	"github.com/lintnet/go-jsonnet-native-functions/pkg/path"
 	"github.com/lintnet/go-jsonnet-native-functions/pkg/path/filepath"
 	"github.com/lintnet/go-jsonnet-native-functions/pkg/regexp"
 	"github.com/lintnet/go-jsonnet-native-functions/pkg/strings"
 )
 
 func SetNativeFunctions(vm *jsonnet.VM) {
-	vm.NativeFunction(strings.Contains("strings.Contains"))
-	vm.NativeFunction(strings.TrimPrefix("strings.TrimPrefix"))
-	vm.NativeFunction(strings.TrimSpace("strings.TrimSpace"))
-	vm.NativeFunction(regexp.MatchString("regexp.MatchString"))
-	vm.NativeFunction(filepath.Base("filepath.Base"))
-	vm.NativeFunction(ValidateJSONSchema("jsonschema.Validate"))
+	m := map[string]func(string) *jsonnet.NativeFunction{
+		"filepath.Base":        filepath.Base,
+		"jsonschema.Validate":  ValidateJSONSchema,
+		"path.Base":            path.Base,
+		"path.Clean":           path.Clean,
+		"path.Dir":             path.Dir,
+		"path.Ext":             path.Ext,
+		"path.IsAbs":           path.IsAbs,
+		"path.Match":           path.Match,
+		"path.Split":           path.Split,
+		"regexp.MatchString":   regexp.MatchString,
+		"strings.Contains":     strings.Contains,
+		"strings.ContainsAny":  strings.ContainsAny,
+		"strings.Count":        strings.Count,
+		"strings.Cut":          strings.Cut,
+		"strings.CutPrefix":    strings.CutPrefix,
+		"strings.CutSuffix":    strings.CutSuffix,
+		"strings.EqualFold":    strings.EqualFold,
+		"strings.Fields":       strings.Fields,
+		"strings.LastIndex":    strings.LastIndex,
+		"strings.LastIndexAny": strings.LastIndexAny,
+		"strings.Repeat":       strings.Repeat,
+		"strings.Replace":      strings.Replace,
+		"strings.TrimPrefix":   strings.TrimPrefix,
+		"strings.TrimSpace":    strings.TrimSpace, //nolint:staticcheck
+		"url.Parse":            url.Parse,
+	}
+	for k, v := range m {
+		vm.NativeFunction(v(k))
+	}
 }
 
 func MakeVM() *jsonnet.VM {
