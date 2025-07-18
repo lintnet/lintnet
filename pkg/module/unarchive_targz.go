52d4318b3c73e824f4048eb2e61a3001112a89b3
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-07-19 08:31:10 +0900

    refactor: split functions

--- pkg/module/unarchive_targz.go
+++ pkg/module/unarchive_targz.go
@@ -27,42 +27,47 @@ func extractTarGz(fs afero.Fs, src, dest string) error {
 		return fmt.Errorf("read a gzip: %w", err)
 	}
 	defer gzr.Close()
-
 	tr := tar.NewReader(gzr)
-
 	for {
-		hdr, err := tr.Next()
-		if errors.Is(err, io.EOF) {
-			return nil
-		}
-		if err != nil {
-			return fmt.Errorf("get a new entry from a tar archive: %w", err)
-		}
-
-		targetPath := filepath.Join(dest, filepath.Clean(hdr.Name))
-
-		switch hdr.Typeflag {
-		case tar.TypeDir:
-			if err := fs.MkdirAll(targetPath, os.FileMode(hdr.Mode)); err != nil { //nolint:gosec
-				return fmt.Errorf("create a directory: %w", err)
-			}
-		case tar.TypeReg:
-			if err := osfile.MkdirAll(fs, filepath.Dir(targetPath)); err != nil {
-				return fmt.Errorf("create a directory: %w", err)
-			}
-
-			outFile, err := fs.OpenFile(targetPath, os.O_CREATE|os.O_RDWR, os.FileMode(hdr.Mode)) //noilnt:gosec
-			if err != nil {
-				return fmt.Errorf("open a file: %w", err)
-			}
-			if _, err := io.CopyN(outFile, tr, maxFileSize); err != nil {
-				outFile.Close()
-				if errors.Is(err, io.EOF) {
-					continue
-				}
-				return fmt.Errorf("copy a file: %w", err)
-			}
-			return errors.New("file size exceeds the limit")
+		if err := readTar(fs, dest, tr); err != nil {
+			return err
 		}
 	}
+}
+
+func readTar(fs afero.Fs, dest string, tr *tar.Reader) error {
+	hdr, err := tr.Next()
+	if errors.Is(err, io.EOF) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("get a new entry from a tar archive: %w", err)
+	}
+
+	targetPath := filepath.Join(dest, filepath.Clean(hdr.Name))
+
+	switch hdr.Typeflag {
+	case tar.TypeDir:
+		if err := fs.MkdirAll(targetPath, os.FileMode(hdr.Mode)); err != nil { //nolint:gosec
+			return fmt.Errorf("create a directory: %w", err)
+		}
+	case tar.TypeReg:
+		if err := osfile.MkdirAll(fs, filepath.Dir(targetPath)); err != nil {
+			return fmt.Errorf("create a directory: %w", err)
+		}
+
+		outFile, err := fs.OpenFile(targetPath, os.O_CREATE|os.O_RDWR, os.FileMode(hdr.Mode)) //nolint:gosec
+		if err != nil {
+			return fmt.Errorf("open a file: %w", err)
+		}
+		if _, err := io.CopyN(outFile, tr, maxFileSize); err != nil {
+			outFile.Close()
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return fmt.Errorf("copy a file: %w", err)
+		}
+		return errors.New("file size exceeds the limit")
+	}
+	return nil
 }
